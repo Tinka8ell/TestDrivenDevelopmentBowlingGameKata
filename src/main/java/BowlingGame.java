bcbcdcc85fafa76d32f47147c62099246dec0e57
Mark Ingamells <mark.ingamells@gmail.com>
2022-04-05 18:16:31 +0100

    RED Expanded detection with simplistic strike (fails) for Bowling Game Kata

--- src/main/java/BowlingGame.java
+++ src/main/java/BowlingGame.java
@@ -1,10 +1,26 @@
 public class BowlingGame {
 
     public static int score(String scores){
-        return scores
-                .chars()
-                .filter(c -> (c >= '0' && c <= '9') || c == 'X')
-                .map(c -> c == 'X' ? 10 : c - '0')
-                .sum();
+        int score = 0;
+        for (char pins: scores.toCharArray()){
+            int pin = pins - '0';
+            switch (pins){
+                case 'X':
+                    pin = 10;
+                    break;
+                case '-':
+                    pin = 0;
+                    break;
+                case ' ':
+                    pin = -1;
+                    break;
+                default:
+                    if (pin < 0 || pin > 9)
+                        pin = -1;
+            }
+            if (pin >= 0)
+                score += pin;
+        }
+        return score;
     }
 }
