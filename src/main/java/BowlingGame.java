ba783ebb2ccf3a77e262348a8048b94c1955234f
Mark Ingamells <mark.ingamells@gmail.com>
2022-04-05 18:08:25 +0100

    RED Simplistic strike (fails) for Bowling Game Kata

--- src/main/java/BowlingGame.java
+++ src/main/java/BowlingGame.java
@@ -1,6 +1,10 @@
 public class BowlingGame {
 
     public static int score(String scores){
-        return scores.chars().filter(c -> c >= '0' && c <= '9').map(c -> c - '0').sum();
+        return scores
+                .chars()
+                .filter(c -> (c >= '0' && c <= '9') || c == 'X')
+                .map(c -> c == 'X' ? 10 : c - '0')
+                .sum();
     }
 }
--- src/test/java/BowlingGameTest.java
+++ src/test/java/BowlingGameTest.java
@@ -28,4 +28,28 @@ class BowlingGameTest {
         assertEquals(20, score, scores);
     }
 
+    @Test
+    public void checkMixed(){
+        // set up
+        String scores = "1- 11 -1 -- 12 11 21 22 11 11"; // 10 frames of 2 singles
+
+        // do it
+        int score = BowlingGame.score(scores);
+
+        // check it
+        assertEquals(20, score, scores);
+    }
+
+    @Test
+    public void checkAllStrikes(){
+        // set up
+        String scores = "X X X X X X X X X X X X "; // 10 frames of strikes and 2 bonus strikes
+
+        // do it
+        int score = BowlingGame.score(scores);
+
+        // check it
+        assertEquals(300, score, scores);
+    }
+
 }
