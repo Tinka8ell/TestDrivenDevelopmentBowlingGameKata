d446bf6e5387648a1d54eab2cb1569a74e072128
Mark Ingamells <mark.ingamells@gmail.com>
2022-04-05 19:06:53 +0100

    GREEN Expanded detection with simplistic strike (fails) for Bowling Game Kata I may have got board and completed the code, but still need to add test cases

--- src/main/java/BowlingGame.java
+++ src/main/java/BowlingGame.java
@@ -1,25 +1,45 @@
+import java.util.ArrayList;
+
 public class BowlingGame {
 
     public static int score(String scores){
         int score = 0;
+        int frameNumber = 0;
+        int bowlsThisFrame = 0;
+        int pinsThisFrame = 0;
+        ArrayList<Character> strikes = new ArrayList<>();
         for (char pins: scores.toCharArray()){
-            int pin = pins - '0';
-            switch (pins){
-                case 'X':
-                    pin = 10;
-                    break;
-                case '-':
-                    pin = 0;
-                    break;
-                case ' ':
-                    pin = -1;
-                    break;
-                default:
-                    if (pin < 0 || pin > 9)
-                        pin = -1;
-            }
-            if (pin >= 0)
+            int pin = "0123456789X/".indexOf(pins); // valid and pin "value"
+            if (pin >= 0) {
+                // a real bowl!
+                if (bowlsThisFrame <= 0) // reset each frame
+                    pinsThisFrame = 10;
+                if (pin > 9) {
+                    // specials
+                    pin = pinsThisFrame; // we cleared them!
+                    bowlsThisFrame = 5; // end of frame!
+                }
                 score += pin;
+                pinsThisFrame -= pin;
+                bowlsThisFrame++;
+                if (bowlsThisFrame >= 2) {
+                    bowlsThisFrame = 0;
+                    frameNumber++;
+                }
+                if (strikes.size() > 0){
+                    // process strikes
+                    ArrayList<Character> spares = new ArrayList<>();
+                    while (strikes.size() > 0){
+                        char first = strikes.remove(0);
+                        score += pin;
+                        if (first == 'X')
+                            spares.add('/'); // return as a spare
+                    }
+                    strikes = spares;
+                }
+                if (pins == 'X' && frameNumber < 10)
+                    strikes.add(pins);
+            }
         }
         return score;
     }
